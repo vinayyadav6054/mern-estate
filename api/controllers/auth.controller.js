f57cda7811598f9f5c84c3b3a6d5311bcba766ed
vinayyadav6054 <yvinay7271@gmail.com>
2024-10-21 08:30:04 +0530

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,6 +9,6 @@ export const signup = async (req, res, next) =>{
         await newUser.save();
         res.status(201).json('User created Successfully');
     } catch (error){
-        next(erro);
+        next(error);
     }  
 };
